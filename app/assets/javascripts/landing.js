a4fe7bfb668590ab6389866192204742b7046500
Dmitry Davydov <haudvd@gmail.com>
2015-06-20 15:49:01 +0300

    add tab on main page

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -2,3 +2,12 @@
 //= require jquery.dropotron.min.js
 //= require jquery.scrollgress.min.js
 //= require skel.min.js
+
+$(function() {
+    $('.tablist .tab a').click(function(){
+        var elId = $(this).data('tab-id');
+        $('.tab-pane').removeClass('active');
+        $('#' + elId).addClass('active');
+        return false;
+    });
+});
