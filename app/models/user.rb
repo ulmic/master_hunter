8df00f0210ac3e0efba58e0cd48555f779737184
Dmitry Davydov <haudvd@gmail.com>
2015-06-20 18:49:28 +0300

    view fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   has_many :posts
+  has_many :authentications
 
   validates :email, email: true,
                     uniqueness: true,
